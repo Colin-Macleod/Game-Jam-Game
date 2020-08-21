473507e5bf1bed698e3ea34aad0ed8f05bbf4480
Liam MacLeod <liammacleod@gmail.com>
2020-08-21 19:00:03 -0400

    Statusbar Sliders

---
+++ Assets/Status.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Status : MonoBehaviour
+{
+
+    public Slider sliderHunger;
+    public Slider sliderThirst;
+    public Slider sliderSunlight;
+
+
+
+    public void SetHunger(int hunger, int thirst, int sun)
+    {
+
+        sliderHunger.value = hunger;
+        sliderThirst.value = thirst;
+        sliderSunlight.value = sun;
+
+    }
+}
+
