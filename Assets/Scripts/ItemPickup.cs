746b6646eec9305d3ddf82e6c7ace51b79343b29
malikovm <mikayil.malikov@gmail.com>
2020-08-22 13:58:47 -0400

    picking up
    you can now pick up objects

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+
+    public Transform objectUp;
+    public bool isHolding = false;
+
+    void OnMouseDown()
+    {
+        if (isHolding == false)
+        {
+            this.transform.position = objectUp.position;
+            this.transform.parent = GameObject.Find("Pickup").transform;
+            isHolding = true;
+        }
+        else if (isHolding == true)
+        {
+            this.transform.parent = null;
+            isHolding = false;
+        }
+    }
+
+}
