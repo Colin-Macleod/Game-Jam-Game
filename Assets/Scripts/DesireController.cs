97035111fc87e96dccf26d4035d915812c6954a1
Sprocket44 <harrisonpelletier@gmail.com>
2020-08-21 22:12:08 -0400

    Happiness increases while near window triggers
    Happiness increases while near window triggers

--- Assets/Scripts/DesireController.cs
+++ Assets/Scripts/DesireController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class DesireController : MonoBehaviour
 {
+    private LayerMask windowLayer;
+    public Status status; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        windowLayer = LayerMask.GetMask("Window");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("collider hit");
+        if (other.gameObject.layer != windowLayer)
+        {
+            //Increase happiness
+            StartCoroutine("IncreaseHappiness");
+            print("start");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == windowLayer)
+        {
+            //Increase happiness
+            StopCoroutine("IncreaseHappiness");
+        }
+    }
+
+    public IEnumerator IncreaseHappiness()
+    {   
+        while(status.happiness < 1)
+        {
+            status.happiness += 0.1f;
+            yield return new WaitForSeconds(2f);
+        }
         
     }
 }
